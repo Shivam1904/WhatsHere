1e80a62dc25ad81e29ccf69f5e7eeb489848bfb5
Shivam1904 <shivam.sriv93@gmail.com>
2015-03-10 01:12:51 +0530

    map added

--- app/src/main/java/com/example/shivam/whatshere/selectedmap.java
+++ app/src/main/java/com/example/shivam/whatshere/selectedmap.java
@@ -4,23 +4,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Shivam on 3/3/2015.
  */
 public class selectedmap extends ActionBarActivity {
     TextView tx;
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.selectedmap);
-
         Intent intent=getIntent();
-        String lat=intent.getStringExtra("lat");
-        String lng=intent.getStringExtra("lng");
+        String lat1=intent.getStringExtra("lat");
+        String lng1=intent.getStringExtra("lng");
 
         tx=(TextView)findViewById(R.id.map_heading);
-        tx.setText(lat+" "+lng);
+        tx.setText(lat1+" "+lng1);
+
+        final LatLng HAMBURG1;
+        Double lat=Double.parseDouble(lat1);
+        Double lng=Double.parseDouble(lng1);
+
+        HAMBURG1 = new LatLng(lat, lng);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG1).title("Hamburg"));
+
+        // Move the camera instantly to hamburg with a zoom of 20.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG1,20));
+
+        // Zoom in, animating the camera.
+        //map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        map.animateCamera(CameraUpdateFactory.zoomTo(13), 2000, null);
 
     }
 }
